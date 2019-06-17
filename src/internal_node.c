69feeea6bc69633c4f7b3120379233eb050917e2
Rafael <rafaelmoa@id.uff.br>
2019-06-17 16:50:32 -0300

    RA: começando a criação da internalNodeCreate

--- src/internal_node.c
+++ src/internal_node.c
@@ -3,11 +3,18 @@
 struct internalNode
 {
     Node;
-    int vet[30];
+    int *IDs;
     bool isPointingToLeaf;
 };
 
-Node *internalNodeCreate(int t);
+Node *internalNodeCreate(int t)
+{
+    extern int ramificationFactor;
+    int keysNumber = (ramificationFactor * 2) - 1;
+    InternalNode *new_node = (InternalNode *)malloc(sizeof(InternalNode));
+    new_node->isPointingToLeaf = false;
+    new_node->IDs = (int *)malloc(sizeof(int) * keysNumber);
+}
 static void *_internalNodeSearch(void *this, int id);
 static Node *_internalNodeInsert(void *this, void *info);
 static Node *_internalNodeRemove(void *this, int id);
