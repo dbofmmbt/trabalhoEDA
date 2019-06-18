9819324df7106c1398641eed78ba9e44e6942f49
Rafael <rafaelmoa@id.uff.br>
2019-06-18 00:39:14 -0300

    RA: Setando referencias de funções internas do nó na criação dele

--- src/internal_node.c
+++ src/internal_node.c
@@ -1,20 +1,31 @@
 #include <internal_node.h>
+extern InfoModel mainModel;
 
 struct internalNode
 {
     Node;
     int *IDs;
     bool isPointingToLeaf;
+    int *children;
 };
 
-Node *internalNodeCreate(int t)
+Node *internalNodeCreate()
 {
     extern int ramificationFactor;
     int keysNumber = (ramificationFactor * 2) - 1;
-    InternalNode *new_node = (InternalNode *)malloc(sizeof(InternalNode));
-    new_node->isPointingToLeaf = false;
-    new_node->IDs = (int *)malloc(sizeof(int) * keysNumber);
+    InternalNode *newNode = (InternalNode *)malloc(sizeof(InternalNode));
+    newNode->isPointingToLeaf = false;
+    newNode->children = malloc(sizeof(int) * ((ramificationFactor * 2) + 1));
+    newNode->nodeSearch = _internalNodeSearch;
+    newNode->nodeInsert = _internalNodeInsert;
+    newNode->nodeRemove = _internalNodeRemove;
+    newNode->nodeGet = _internalNodeGet;
+    newNode->nodeLenght = _internalNodeLenght;
+    newNode->nodeSizeInBytes = _internalNodeSizeInBytes;
 }
 static void *_internalNodeSearch(void *this, int id);
 static Node *_internalNodeInsert(void *this, void *info);
 static Node *_internalNodeRemove(void *this, int id);
+static Node *_internalNodeGet(void *this, int i);
+static int _internalNodeLenght(void *this);
+static int _internalNodeSizeInBytes(void *this);
