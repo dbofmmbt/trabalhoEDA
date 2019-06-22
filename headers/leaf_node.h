4eadebe1e943757e361aaaef07947ad229f6ec4f
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2019-06-22 20:11:22 -0300

    LeafNodeLoad finalizado

--- headers/leaf_node.h
+++ headers/leaf_node.h
@@ -7,4 +7,4 @@ LeafNode *leafNodeCreate(int t);
 /* if pos = -1, saves on the end of file. Returns the position where the node is saved. */
 Address leafNodeStore(LeafNode *node, Address pos);
 
-LeafNode *leafNodeLoad(Address pos); // TODO
+LeafNode *leafNodeLoad(Address pos);
