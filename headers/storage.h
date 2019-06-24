c79978e6681c39cfa79efde681014f4266bdcef5
Rafael <rafaelmoa@id.uff.br>
2019-06-24 15:06:36 -0300

    RA: movendo paths globais de arquivos para utils.h

--- headers/storage.h
+++ headers/storage.h
@@ -4,11 +4,6 @@
 #include <leaf_node.h>
 #include <internal_node.h>
 
-#define SEC_INDEX_FILE_PATH "data/sec_index.bin"
-#define MAIN_INDEX_FILE_PATH "data/main_index.bin"
-#define DATA_FILE_PATH "data/data.bin"
-#define METADATA_FILE_PATH "data/metadata.bin"
-
 int ramificationFactor; // ? Does it make sense to keep it on metadata?
 
 void setupStorage(char *catalogName, int degree);
--- headers/utils.h
+++ headers/utils.h
@@ -4,3 +4,8 @@
 #include <string.h>
 
 typedef int Address;
+
+#define SEC_INDEX_FILE_PATH "data/sec_index.bin"
+#define MAIN_INDEX_FILE_PATH "data/main_index.bin"
+#define DATA_FILE_PATH "data/data.bin"
+#define METADATA_FILE_PATH "data/metadata.bin"
