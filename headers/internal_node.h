290e4a281ac0f285f4172b7ceba36c49ed28c12e
Rafael <rafaelmoa@id.uff.br>
2019-06-24 15:11:00 -0300

    RA: criando todas as funções de internal_node

--- headers/internal_node.h
+++ headers/internal_node.h
@@ -1,5 +1,22 @@
 #include <utils.h>
+#include <info_model.h>
 
-typedef struct internalNode InternalNode;
+typedef struct IN
+{
+    int numberOfKeys;
+    int *IDs;
+    Address *children;
+} internalNode;
 
-InternalNode * internalNodeCreate(int t);
+//cria um nó interno na MP e retorna um ponteiro pra ele
+internalNode *internalNodeCreate();
+
+/*guarda um nó no arquivo de indices e retorna o endereço onde foi salvo no arquivo.
+para inserir no final do arquivo, passe como parâmetro -1 para a Posição de inserção*/
+Address internalNodeStore(internalNode *node, Address insertPosition);
+
+//carrega um nó interno pra memória principal e retorna um ponteiro pra ele na MP
+internalNode *internalNodeLoad(Address nodePosition);
+
+//libera um nó na MP
+void internalNodeFree(internalNode *node);
--- src/internal_node.c
+++ src/internal_node.c
@@ -1,34 +1,95 @@
 #include <internal_node.h>
+
 extern InfoModel mainModel;
+extern int ramificationFactor;
 
-struct internalNode
+//cria um nó interno na MP e retorna um ponteiro pra ele
+internalNode *internalNodeCreate()
 {
-    Node;
-    int *IDs;
-    bool isPointingToLeaf;
-    int *children;
-};
+    int maxNumberOfKeys = (ramificationFactor * 2) - 1;
+    int maxNumberOfChildren = ((ramificationFactor * 2));
 
-Node *internalNodeCreate()
-{
-    extern int ramificationFactor;
-    int keysNumber = (ramificationFactor * 2) - 1;
-    InternalNode *newNode = (InternalNode *)malloc(sizeof(InternalNode));
-    newNode->isPointingToLeaf = false;
-    newNode->children = malloc(sizeof(int) * ((ramificationFactor * 2) + 1));
-    newNode->nodeSearch = _internalNodeSearch;
-    newNode->nodeInsert = _internalNodeInsert;
-    newNode->nodeRemove = _internalNodeRemove;
-    newNode->nodeGet = _internalNodeGet;
-    newNode->nodeLenght = _internalNodeLenght;
-    newNode->nodeSizeInBytes = _internalNodeSizeInBytes;
+    internalNode *newNode = (internalNode *)malloc(sizeof(internalNode));
+
+    newNode->IDs = (int *)malloc(sizeof(int) * maxNumberOfKeys);
+    newNode->children = (Address *)malloc(sizeof(Address) * maxNumberOfChildren);
+
+    newNode = internalNodeInitialize(newNode);
+
+    return newNode;
 }
-static void *_internalNodeSearch(void *this, int id){
-    //tenta achar um ID em um nó interno
 
-};
-static Node *_internalNodeInsert(void *this, void *info);
-static Node *_internalNodeRemove(void *this, int id);
-static Node *_internalNodeGet(void *this, int i);
-static int _internalNodeLenght(void *this);
-static int _internalNodeSizeInBytes(void *this);
+static internalNode *internalNodeInitialize(internalNode *node)
+{
+
+    int maxNumberOfKeys = (ramificationFactor * 2) - 1;
+    int maxNumberOfChildren = ((ramificationFactor * 2));
+    //numero de chaves iniciando com 0
+    node->numberOfKeys = 0;
+    //todos os ID's sendo setado como 0
+    for (int i = 0; i < maxNumberOfKeys; i++)
+    {
+        node->IDs[i] = 0;
+    }
+    //todas no vetor de filhos apontando pra -1 (não apontando pra nenhum nó)
+    for (int i = 0; i < maxNumberOfChildren; i++)
+    {
+        node->children[i] = -1;
+    }
+
+    return node;
+}
+
+/*guarda um nó no arquivo de indices e retorna o endereço onde foi salvo no arquivo.
+para inserir no final do arquivo, passe como parâmetro -1 para a Posição de inserção*/
+Address internalNodeStore(internalNode *node, Address insertPosition)
+{
+    int maxNumberOfKeys = (ramificationFactor * 2) - 1;
+    int maxNumberOfChildren = ((ramificationFactor * 2));
+
+    FILE *archive = fopen(MAIN_INDEX_FILE_PATH, "rb+");
+
+    if (insertPosition < 0)
+    {
+        fseek(archive, 0, SEEK_END);
+        insertPosition = ftell(archive);
+    }
+    else
+    {
+        fseek(archive, insertPosition, SEEK_SET);
+    }
+
+    fwrite(node->numberOfKeys, sizeof(int), 1, archive);
+    fwrite(node->IDs, sizeof(int), maxNumberOfKeys, archive);
+    fwrite(node->children, sizeof(Address), maxNumberOfChildren, archive);
+
+    fclose(archive);
+
+    return insertPosition;
+}
+//carrega um nó interno pra memória principal e retorna um ponteiro pra ele na MP
+internalNode *internalNodeLoad(Address nodePosition)
+{
+
+    int maxNumberOfKeys = (ramificationFactor * 2) - 1;
+    int maxNumberOfChildren = ((ramificationFactor * 2));
+
+    internalNode *node = internalNodeCreate();
+    FILE *archive = fopen(MAIN_INDEX_FILE_PATH, "rb");
+
+    fseek(archive, nodePosition, SEEK_SET);
+    fread(node->numberOfKeys, sizeof(int), 1, archive);
+    fread(node->IDs, sizeof(int), maxNumberOfKeys, archive);
+    fread(node->children, sizeof(Address), maxNumberOfChildren, archive);
+
+    fclose(archive);
+
+    return node;
+}
+//libera um nó na MP
+void internalNodeFree(internalNode *node)
+{
+    free(node->IDs);
+    free(node->children);
+    free(node);
+}
