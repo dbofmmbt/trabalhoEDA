0c570aa0c9e00f5ea5e45ed083831428f79864f9
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2019-06-28 07:04:52 -0300

    Desabilitados arquivos sec_index

--- headers/sec_index/sec_index_storage.h
+++ headers/sec_index/sec_index_storage.h
@@ -4,10 +4,14 @@
 #include <utils.h>
 #include <info_model.h>
 
-void addElement(char *element); // TODO
+/*
+    THIS FILE WON'T BE IMPLEMENTED BY NOW.
+*/
+
+void addElement(char *element);
 /* It gets all elements registered on the system */
-char **getAllElementsName(void); // TODO
+char **getAllElementsName(void);
 /* Size of the array of the element's names */
-int getAllElementsCount(void); // TODO
+int getAllElementsCount(void);
 
 #endif
--- headers/sec_index/sec_index_user.h
+++ headers/sec_index/sec_index_user.h
@@ -4,13 +4,17 @@
 #include <utils.h>
 #include <info_model.h>
 
+/*
+    THIS FILE WON'T BE IMPLEMENTED BY NOW.
+*/
+
 /* The name convention in sec_index is:
     * Element: A value of the secondary index
     * Related: A info that belongs to a specific Element
 */
 
-void *getAllRelated(char *element); // TODO
+void *getAllRelated(char *element);
 
-void deleteAllRelated(char *element); // TODO
+void deleteAllRelated(char *element);
 
 #endif
